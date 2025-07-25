ea192cef4a07e94b84ae28f1d3cfd8463feb4485
TwoFace <Themasterpo69@gmail.com>
2025-07-25 14:29:09 +0300

    Updates

--- get_sticker_id.py
+++ get_sticker_id.py
@@ -1,10 +1,12 @@
 import random
+import os
+import dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes, MessageHandler, filters
 
+dotenv.load_dotenv()
 
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
-
+TOKEN = os.getenv("TOKEN")
 
 async def handle_sticker(update: Update, context: ContextTypes.DEFAULT_TYPE):
     sticker = update.message.sticker
@@ -12,6 +14,6 @@ async def handle_sticker(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f"Received sticker: {sticker.file_id}")
 
 
-app = Application.builder().token("7722655180:AAHmAhYvwVBZ_-H5CBHw6FONEPc4ggCCXIg").build()
+app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.Sticker.ALL, handle_sticker))
 app.run_polling()
